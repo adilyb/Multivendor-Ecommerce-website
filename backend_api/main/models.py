9ffbdd1842a8bc0f508794f657ad92631519cd6a
adilyb <muhammedadilyb@gmail.com>
2024-02-13 01:22:10 +0530

    initial commit

---
+++ backend_api/main/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Vendor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.TextField(null=True)
+    
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=200)
+    detail = models.TextField(null=True)
+    
+    
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    detail = models.TextField(null=True)
+    price = models.FloatField()
